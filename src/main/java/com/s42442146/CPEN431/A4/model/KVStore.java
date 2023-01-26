7e0d438488dc4598a823ae0e126a99bc1f479b82
Megan Ma <menghanma@gmail.com>
2023-01-26 12:42:16 -0800

    concurrent server

---
+++ src/main/java/com/s42442146/CPEN431/A4/model/KVStore.java
@@ -0,0 +1,29 @@
+package com.s42442146.CPEN431.A4.model;
+
+import ca.NetSysLab.ProtocolBuffers.Value;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class KVStore {
+    private ConcurrentHashMap<ByteBuffer, Value.Val> store;
+    private final static KVStore instance = new KVStore();
+
+    private KVStore() {
+        store = new ConcurrentHashMap<>();
+    }
+
+    public static KVStore getInstance() {
+        return instance;
+    }
+
+    public ConcurrentHashMap<ByteBuffer, Value.Val> getStore() {
+        return store;
+    }
+
+    public  void clearStore () {
+        this.store.clear();
+        this.store = new ConcurrentHashMap<>();
+        System.gc();
+    }
+}
---
+++ src/test/java/com/s42442146/CPEN431/A4/ClientThread.java
@@ -0,0 +1,4 @@
+package com.s42442146.CPEN431.A4;
+
+public class ClientThread {
+}
